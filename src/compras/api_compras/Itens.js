4d343e559da0fe5ddee1da991dd5332d799d30a1
unknown <marlonmazzine@gmai.com>
2020-11-23 21:20:40 -0300

    inserindo limite para obter os itens da compra

--- src/compras/api_compras/Itens.js
+++ src/compras/api_compras/Itens.js
@@ -5,6 +5,8 @@ export default class Itens extends React.Component {
     async obterItens(linkDoItem) {
         const URL = process.env.REACT_APP_TRATAMENTO_CORS + process.env.REACT_APP_URL_API_COMPRAS + linkDoItem
         var respostaDaRequisicao
+        const tentativasMaximas = 20
+        var tentativas = 0
 
         do {
             respostaDaRequisicao = await new FetchGet().obterRespostaFetchGetEmJson(URL)
@@ -12,7 +14,17 @@ export default class Itens extends React.Component {
             if (respostaDaRequisicao === 404) {
                 return linkDoItem
             }
-        } while (respostaDaRequisicao === '')
+
+            if (respostaDaRequisicao === '') {
+                tentativas++
+            } else {
+                break;
+            }
+        } while (tentativas < tentativasMaximas)
+
+        if (tentativas === tentativasMaximas) {
+            throw new Error('Não foi possível obter os itens da compra atual no momento. Aguarde um instante para atualizar novamente.')
+        }
 
         return await respostaDaRequisicao._embedded.compras
     }
